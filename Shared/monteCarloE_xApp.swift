841f6619c232d589acf6f9f04ce66c24fc834314
Matt <madas@hawk.iit.edu>
2021-02-19 10:25:33 -0600

    Initial Commit

---
+++ Shared/monteCarloE_xApp.swift
@@ -0,0 +1,17 @@
+//
+//  monteCarloE_xApp.swift
+//  Shared
+//
+//  Created by Matthew Adas on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct monteCarloE_xApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
